d9f5a3018adbe2e6db0aa56333bb7ff1e6ee9694
abdulkareem <bdlkrmalabi@gmail.com>
2025-07-10 14:56:39 +0100

    restructure image folder

---
+++ lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF007BFF);
+  static const Color bgColor = Color(0xff7A1313);
+  static const Color lightYellow = Color(0xffffe082);
+}
